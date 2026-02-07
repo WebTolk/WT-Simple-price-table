c4097623efe8d577c88ec507ab41182c366b1c8d
Сергей С. <87950098+Gresserg@users.noreply.github.com>
2026-02-07 12:02:02 +0300

    Первый коммит.

--- tmpl/default.php
+++ tmpl/default.php
@@ -22,48 +22,47 @@ if (empty($sections)) return;
 <div class="d-flex flex-column">
    <?php foreach ($sections as $section) : ?>
        <details class="p-3 bg-light border border-light mb-2 shadow-sm">
-           <summary class="py-2" itemprop="name">
+           <summary class="py-2">
                <h3 class="d-inline h5"><?php echo $section->section_title; ?></h3>
            </summary>
            <?php if (!empty($section->table_rows)): ?>
                <table class="table">
                    <?php foreach ($section->table_rows as $row) : ?>
-                       <tr itemscope itemtype="https://schema.org/Product">
-                           <?php
-                           if(!empty($row->product_icon)) { ?>
-                               <td>
-                                   <?php $img_src = HTMLHelper::cleanImageURL($row->product_icon);
-                                   $img_attribs = [
+                       <?php if (!empty($row->product_title)) : ?>
+                           <tr>
+                               <td  class="align-middle w-auto">
+                                   <?php if(!empty($row->product_icon)) : ?>
+                                   <div class="d-flex gap-3 align-items-center">
+                                       <?php $img_src = HTMLHelper::cleanImageURL($row->product_icon);
+                                       $img_attribs = [
                                            'width' => '72',
-                                       'height' => '72',
-                                       'style' => 'margin-bottom: -4px;',
-                                       'class' => 'me-4'
-                                   ];
-                                   echo HTMLHelper::image($img_src->url,
-                                       (!empty($row->product_title) ? htmlspecialchars($row->product_title):'иконка'), $img_attribs); ?>
+                                           'style' => 'margin-bottom: -4px;',
+                                           ];
+                                           $img_alt = $row->product_title;
+                                           echo HTMLHelper::image($img_src->url, $img_alt, $img_attribs); ?>
+                                        <?php endif ?>
+                                        <p><?php echo $row->product_title; ?></p>
+                                   </div>
+                                   </td>
+                               <!--<td><p><?php // echo $row->product_title; ?></p></td>-->
+                               <td class="align-middle w-25 text-end">
+                                   <p>
+                                       <?php if (!empty($row->old_price)) :?>
+                                           <s class="text-muted fs-7 me-2"><?php echo $row->old_price; ?></s>
+                                       <?php endif; ?>
+                                       <?php if (!empty($row->show_from)) :
+                                                 echo "<span class=\"me-1\">от</span>";
+                                            endif; ?>
+                                       <?php if (!empty($row->price)) :?>
+                                           <span class="me-1"><?php echo $row->price; ?></span>
+                                       <?php endif; ?>
+                                       <?php if (!empty($currency)) :?>
+                                           <span class=""><?php echo $currency; ?></span>
+                                       <?php endif; ?>
+                                   </p>
                                </td>
-                           <?php } ?>
-
-                           <td class="w-75" itemprop="name"><p><?php echo $row->product_title; ?></p></td>
-                           <td class="text-center" itemprop="offers" itemscope itemtype="https://schema.org/Offer">
-                               <meta itemprop="price" content="<?php echo $row->price; ?>">
-                               <meta itemprop="priceCurrency" content="<?php echo $currency; ?>">
-                               <p>
-                                   <?php if (!empty($row->old_price)) :?>
-                                       <s class="text-muted fs-7 me-2"><?php echo $row->old_price; ?></s>
-                                   <?php endif; ?>
-                                   <?php if (!empty($row->show_from)) :
-                                             echo "<span class=\"me-1\">от</span>";
-                                        endif; ?>
-                                   <?php if (!empty($row->price)) :?>
-                                       <span class="me-1"><?php echo $row->price; ?></span>
-                                   <?php endif; ?>
-                                   <?php if (!empty($currency)) :?>
-                                       <span class=""><?php echo $currency; ?></span>
-                                   <?php endif; ?>
-                               </p>
-                           </td>
-                       </tr>
+                           </tr>
+                       <?php endif; ?>
                    <?php endforeach; ?>
                </table>
            <?php endif; ?>
